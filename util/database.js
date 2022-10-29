87d48449ad5e870506bce34ad1db0b24150729c6
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2022-10-29 14:31:33 +0900

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node-complete", "root", "pms1227", {
+const sequelize = new Sequelize("node-complete", "root", {//password required next to root
   dialect: "mysql",
   host: "localhost",
 });
